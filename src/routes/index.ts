9f72e4ddd8b541befa447a1c5c8ce952545e31db
Paracefas <emanuelclur2001@outlook.com>
2019-08-06 09:09:09 -0300

    Add main_condition to json res and add support to es and en languages

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,9 +8,9 @@ router.get('/', (req, res) => {
     res.send('<h1>Api-Clima | Home</h1>')
 })
 
-router.get('/:lat&:lon&:key', async (req, res) => {
-    const { lat, lon, key } = req.params
-    res.json(await getCurrent(key, new WeatherMap(lat, lon)))
+router.get('/:lat&:lon&:lang&:key', async (req, res) => {
+    const { lat, lon, key, lang } = req.params
+    res.json(await getCurrent(key, new WeatherMap(lat, lon, lang)))
 })
 
 export default router
